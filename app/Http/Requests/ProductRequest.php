661f1096f39ed057883e8b8dc45ab30258ed71df
quoctv <vanquoc26520@gmail.com>
2023-12-17 10:33:47 +0700

    add col image master product

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -22,7 +22,7 @@ class ProductRequest extends BaseRequest
                 'quantity' => [Rule::requiredIf(!$this->id),'nullable', 'numeric', 'min:0'],
                 'status' => ['nullable', 'numeric', Rule::in([Product::STATUS_UN_APPROVE, PRODUCT::STATUS_APPROVE, Product::STATUS_OUT_STOCK])],
                 'price' => [Rule::requiredIf(!$this->id),'nullable', 'numeric', 'min:0'],
-                'image_master' => ['nullable', 'array'],
+                'image_master' => ['nullable', 'string'],
                 'image' => ['nullable', 'array'],
                 'image.*' => ['nullable', 'string'],
                 'description' => ['nullable', 'string'],
